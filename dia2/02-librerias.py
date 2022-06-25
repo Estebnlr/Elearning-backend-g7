4144450fd719313e4a41ab1d1e5e666b42e8a239
Estebnlr <99986563+Estebnlr@users.noreply.github.com>
2022-06-24 21:30:08 -0500

    que esta pasando aqui

---
+++ dia2/02-librerias.py
@@ -0,0 +1,12 @@
+from camelcase import CamelCase
+
+camelcase = CamelCase()
+
+parrafo = 'hola amigos veamos si esta libreria funciona'
+
+resultado = camelcase.hump(parrafo)
+print(resultado)
+
+# Patron de Diseño Singleton
+# TODO: Replicar el funcionamiento de la libreria 
+
